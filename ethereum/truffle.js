6692d7ab673c67db197538dd97a1bb942ddb5c01
kvdenden <ken@enden.be>
2018-04-18 20:52:04 +1000

    Setup rinkeby network deployment.

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -1,4 +1,23 @@
+require('dotenv').config()
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = process.env.HDWALLET_MNEMONIC;
+var providerUri = "https://rinkeby.infura.io/" + process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, providerUri)
+      },
+      network_id: 4
+    }
+  }
 };
