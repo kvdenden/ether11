d09b49189539450e7e58166466880b7784116ad3
kvdenden <ken@enden.be>
2018-04-18 20:56:11 +1000

    Point client to deployed contract.

--- client/lib/web3.js
+++ client/lib/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-const providerUri = process.env.PROVIDER_URI || `http://localhost:9545`;
+const providerUri = "https://rinkeby.infura.io/" + process.env.INFURA_ACCESS_TOKEN;
 const provider = new Web3.providers.HttpProvider(providerUri);
 
 const web3 = new Web3(provider);
