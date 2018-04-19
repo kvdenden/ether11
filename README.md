da7afb9caf7bb8ffe64b037dd761dec7f63a45c9
kvdenden <ken@enden.be>
2018-04-19 23:54:16 +1000

    Add ganache-cli and truffle docker containers.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Ether 11
 
 A collectible trading card game on the blockchain
+
+## How to run
+
+```
+docker-compose run --rm truffle compile
+docker-compose up -d ganache
+docker-compose run --rm truffle migrate
+docker-compose up client
+```
--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -1,20 +1,18 @@
-require('dotenv').config()
 var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var mnemonic = process.env.HDWALLET_MNEMONIC;
-var providerUri = "https://rinkeby.infura.io/" + process.env.INFURA_ACCESS_TOKEN;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: 'localhost',
-      port: 9545,
+      host: process.env.RPC_HOST || 'localhost',
+      port: 8545,
       network_id: '*' // Match any network id
     },
     rinkeby: {
       provider: function() {
+        var mnemonic = process.env.HDWALLET_MNEMONIC;
+        var providerUri = "https://rinkeby.infura.io/" + process.env.INFURA_ACCESS_TOKEN;
         return new HDWalletProvider(mnemonic, providerUri)
       },
       network_id: 4
